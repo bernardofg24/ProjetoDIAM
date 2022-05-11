7df203a137f75d13e56e8f9a35b4c8dc7459254c
bfgoa1-iscte <bfgoa1@iscte-iul.pt>
2022-05-11 04:10:56 +0100

    Starting models

--- projeto/website/models.py
+++ projeto/website/models.py
@@ -1,3 +1,31 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Utilizador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img_src = models.CharField(max_length=150)
+
+class JogosUtilizador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Jogos(models.Model):
+    jogos_user = models.ForeignKey(JogosUtilizador, on_delete=models.CASCADE)
+
+    nome = models.CharField(max_length=50)
+    img_src = models.CharField(max_length=150)
+    dev = models.CharField(max_length=50)
+    pub = models.CharField(max_length=50)
+    release = models.DateTimeField('release date')
+
+class Generos(models.Model):
+    generos_pref = models.ForeignKey(Utilizador, on_delete=models.SET_NULL, null=True)
+    genero_jogo = models.OneToOneField(Jogos, on_delete=models.RESTRICT)
+
+    genero = models.CharField(max_length=50)
+
+class Review(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    jogo = models.OneToOneField(Jogos, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(1)])
+    comment = models.CharField(max_length=500)
