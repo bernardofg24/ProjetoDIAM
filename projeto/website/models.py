dab899d999c9fee2d3748dd87428e45c95897f10
bfgoa1-iscte <bfgoa1@iscte-iul.pt>
2022-05-11 20:06:26 +0100

    Models v2

--- projeto/website/models.py
+++ projeto/website/models.py
@@ -2,30 +2,38 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 
-class Utilizador(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img_src = models.CharField(max_length=150)
 
-class JogosUtilizador(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Generos(models.Model):
+    genero = models.CharField(max_length=50, primary_key=True)
 
-class Jogos(models.Model):
-    jogos_user = models.ForeignKey(JogosUtilizador, on_delete=models.CASCADE)
 
+class Jogo(models.Model):
     nome = models.CharField(max_length=50)
     img_src = models.CharField(max_length=150)
     dev = models.CharField(max_length=50)
     pub = models.CharField(max_length=50)
-    release = models.DateTimeField('release date')
+    release = models.DateTimeField('Release Date')
 
-class Generos(models.Model):
-    generos_pref = models.ForeignKey(Utilizador, on_delete=models.SET_NULL, null=True)
-    genero_jogo = models.OneToOneField(Jogos, on_delete=models.RESTRICT)
 
-    genero = models.CharField(max_length=50)
+class GeneroJogo(models.Model):
+    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Generos, on_delete=models.CASCADE)
+
+
+class Utilizador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    img_src = models.CharField(max_length=150)
+    generos_pref = models.ForeignKey(Generos, on_delete=models.SET_NULL, null=True)
+
+
+class JogosUtilizador(models.Model):
+    user = models.OneToOneField(Utilizador, on_delete=models.CASCADE, primary_key=True)
+    jogos = models.ForeignKey(Jogo, on_delete=models.CASCADE)
+
 
 class Review(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    jogo = models.OneToOneField(Jogos, on_delete=models.CASCADE)
+    user = models.OneToOneField(Utilizador, on_delete=models.CASCADE)
+    jogo = models.OneToOneField(Jogo, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(1)])
     comment = models.CharField(max_length=500)
+
